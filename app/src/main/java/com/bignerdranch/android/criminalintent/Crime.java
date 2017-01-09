fef8ece54323b21bd4bc7852b15ec235bf70fa57
CatArthur <mssizov@gmail.com>
2017-01-09 18:10:02 +0300

    09.01.16_18:09

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID mid;
+    private String mTitle;
+    public Crime(){
+        mid=UUID.randomUUID();
+    }
+    public String getTitle(){
+        return mTitle;
+    }
+    public void setTitle(String title){
+        mTitle=title;
+    }
+}
